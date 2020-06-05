a63252d84288321c69294b6ee15d539bb561f282
Wesley Pestana <wesley.pestana@hotmail.com>
2020-06-05 17:06:07 -0300

    starter day 5

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const server = express()
 
+const db = require('./database/db')
+
 server.use(express.static('public'))
+
+server.use(express.urlencoded({ extended: true }))
 
 // Nunjucks config
 const nunjucks = require('nunjucks')
@@ -18,8 +22,46 @@ server.get('/create-point', (req, res) => {
   return res.render('create-point.html')
 })
 
-server.get('/search-results', (req, res) => {
-  return res.render('search-results.html')
+server.post('/create-point', (req, res) => {
+  const query = `
+    INSERT INTO places (image, name, address, address2, state, city, items) 
+    VALUES (?, ?, ?, ?, ?, ?, ?);
+  `
+  const values = [
+    req.body.image,
+    req.body.name,
+    req.body.address,
+    req.body.address2,
+    req.body.state,
+    req.body.city,
+    req.body.items,
+  ]
+
+  db.run(query, values, function(err) {
+      if (err) {
+        return console.log(err)
+      }
+      console.log('Cadastrado com sucesso')
+      console.log(this)
+
+      return res.render('create-point.html', { saved: true })
+    })
+
 })
+
+server.get('/search-results', (req, res) => {
+  const search = req.query.search
+  
+  if (search == '') {
+    return res.render('search-results.html', { total: 0 })
+  }
+  db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function(err, rows) {
+    if (err) {
+      return console.log(err)
+    }
+    return res.render('search-results.html', { places: rows, total: rows.length })
+  })
+})
+
 
 server.listen(3000)
